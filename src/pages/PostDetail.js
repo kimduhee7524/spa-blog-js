19fdd6fe875cb854eec8e849a64ef051a9b38d5d
duhee <duhee@groufin.com>
2025-03-21 21:03:40 +0900

    feat: 동적 라우팅에 따른 게시글 상세 페이지 연동

--- src/pages/PostDetail.js
+++ src/pages/PostDetail.js
@@ -1,10 +1,26 @@
-export function PostDetail(id) {
+import { getPostDetails } from "../services/postService.js";
+
+export function PostDetail(Id) {
+  console.log("id", Id);
+  const postDetail = getPostDetails(parseInt(Id));
+
   return `
-      <div class="max-w-md w-full">
-        <div class="bg-white p-6 rounded-lg shadow-lg max-w-4xl mx-auto my-8">
-          <h1 class="text-3xl font-bold mb-2">PostDetail 페이지</h1>
-          <h3 class="text-3xl font-bold mb-2">Post ID: ${id}</h3>
+    <div class="container mx-auto p-4">
+      <div class="bg-white p-6 rounded-lg shadow-lg max-w-4xl mx-auto my-8">
+        <button class="mb-4 bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded" 
+                onclick="window.router.navigateTo('/')">뒤로가기</button>
+
+        <h1 class="text-3xl font-bold mb-2">${postDetail.title}</h1>
+
+        <div class="items-center mb-6 text-gray-600 text-sm">
+          <div class="mr-4">${postDetail.author}</div>
+          <div>작성일: ${postDetail.date}</div>
+        </div>
+
+        <div class="prose max-w-none">
+          ${postDetail.content}
         </div>
       </div>
-    `;
+    </div>
+  `;
 }
